c8d226f42632adcd3a2c807989a61fd992934b8a
Chance Crosson <chancecrosson@gmail.com>
2026-03-05 10:20:27 -0600

    Index navbar modifications. Added navbar to resume.

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('nav-toggle');
+  const menu = document.getElementById('nav-menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+  });
+});
