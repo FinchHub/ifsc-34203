5fa8c216b5e2b3f9ddb2f92d840225ebb68e7a02
Chance <chancecrosson@gmail.com>
2026-04-24 15:17:03 -0500

    Added touchmove function to close nav when moving on mobile

--- final-project/assets/js/nav-script.js
+++ final-project/assets/js/nav-script.js
@@ -2,6 +2,10 @@
 // minimize the nav when the DOM itself is tapped/clicked. without stopPropagation,
 // it'll just immediately open and close the nav since it's technically clicking the
 // DOM.
+//
+// The more I tested the site the more I had to go back and add, like when previewing
+// the site on mobile, I noticed the menu didn't go away if the user slid the screen
+// rather than tapping, so that was added via touchmove.
 
 const toggle = document.getElementById('nav-btn');
 const navMenu = document.getElementById('nav-link-items');
@@ -19,3 +23,11 @@ document.addEventListener('click', (e) => {
         navbar.classList.remove('open');
     }
 });
+
+document.addEventListener('touchmove', (e) => {
+    if (!navMenu.contains(e.target)) {
+        navMenu.classList.remove('open');
+        navbar.classList.remove('open');
+    }
+});
+
